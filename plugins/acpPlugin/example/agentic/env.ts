4402ccdf75aa2876368c45f7618b40d864d3d8d6
Joey Lau <joeying0712@gmail.com>
2025-06-29 22:52:46 +0800

    refactor: update buyer and seller environment variable name

--- plugins/acpPlugin/example/agentic/env.ts
+++ plugins/acpPlugin/example/agentic/env.ts
@@ -13,12 +13,11 @@ function getEnvVar<T extends string = string>(key: string, required = true): T {
 
 // ACP Agents' Credentials
 export const WHITELISTED_WALLET_PRIVATE_KEY = getEnvVar<Address>('WHITELISTED_WALLET_PRIVATE_KEY');
-export const WHITELISTED_WALLET_ENTITY_ID = parseInt(getEnvVar('WHITELISTED_WALLET_ENTITY_ID'), 10);
+export const SELLER_ENTITY_ID = parseInt(getEnvVar('SELLER_ENTITY_ID'), 10);
+export const BUYER_ENTITY_ID = parseInt(getEnvVar('BUYER_ENTITY_ID'), 10);
 export const BUYER_AGENT_WALLET_ADDRESS = getEnvVar('BUYER_AGENT_WALLET_ADDRESS') as Address;
 export const SELLER_AGENT_WALLET_ADDRESS = getEnvVar<Address>('SELLER_AGENT_WALLET_ADDRESS') as Address;
 
-// GAME API Key
-export const GAME_DEV_API_KEY = getEnvVar('GAME_DEV_API_KEY');
 // GAME Dev API Key
 export const GAME_API_KEY = getEnvVar('GAME_API_KEY');
 
@@ -36,6 +35,10 @@ export const SELLER_AGENT_GAME_TWITTER_ACCESS_TOKEN = getEnvVar('SELLER_AGENT_GA
 // export const SELLER_AGENT_TWITTER_ACCESS_TOKEN = getEnvVar('SELLER_AGENT_TWITTER_ACCESS_TOKEN');
 // export const SELLER_AGENT_TWITTER_ACCESS_TOKEN_SECRET = getEnvVar('SELLER_AGENT_TWITTER_ACCESS_TOKEN_SECRET');
 
-if (isNaN(WHITELISTED_WALLET_ENTITY_ID)) {
-    throw new Error('WHITELISTED_WALLET_ENTITY_ID must be a valid number in the .env file');
+if (isNaN(BUYER_ENTITY_ID)) {
+    throw new Error('BUYER_ENTITY_ID must be a valid number in the .env file');
+}
+
+if (isNaN(SELLER_ENTITY_ID)) {
+    throw new Error('SELLER_ENTITY_ID must be a valid number in the .env file');
 }
